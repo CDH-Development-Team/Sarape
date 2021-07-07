99c1202111333be9f1aa9dceccd788ab44aba7cb
Eli Acuña <eliacuna@dhcp-10-132-247-95.uawifi.arizona.edu>
2021-07-06 17:50:56 -0700

    tourism code migrated to /tourismSection

---
+++ src/components/TourismSection/Data.js
@@ -0,0 +1,72 @@
+import img1 from "../../images/tourismInMexico.jpg";
+import img2 from "../../images/candids.jpg";
+
+export const tourismObjOne = {
+    id : 'SectionOne',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '',
+    headline: "Tourism and the Saltillo",
+    description: "The Mexican nationalist movement of the early 1900s, appropriated the country’s Spanish and Indigenous identities to create a hybrid “Mexican” identity. The Saltillo sarape turned out to be one of the country’s most enduring symbols. This period saw the rise of the rainbow-striped blanket, which became synonymous with Mexico through its use in films, posters, and advertising. ",
+    description2:"As tourism to Mexico increased, many visitors purchased these colorful sarapes as souvenirs. With demand on the rise, sarape-manufacturing plants were established. Through the use of electric-powered looms, synthetic dyes, and acrylic yarns, production time was shortened and the number of textiles increased. As a result, by the 1930s, there was a drastic decline in handwoven sarapes and the use of natural-dyed yarns.",
+    buttonLabel: "",
+    imgStart: false,
+    img: img1,
+    alt: 'Paper',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const tourismObjTwo = {
+    id : 'SectionTwo',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: '',
+    headline: "Tourism and the Saltillo",
+    description: "Mexican ceramic artists in the area of Guadalajara responded to the tourist market by creating miniature scenes of Mexican people from the 1800s. Often, the male figures who rode or worked with horses were depicted wearing Saltillo sarapes.",
+    description2:"The attire of the two male figures wrapped in Saltillo sarapes indicates they are part of the upper class. The vaquero carries a sarape rolled up behind his saddle. ",
+    buttonLabel: "",
+    imgStart: true,
+    img: img1,
+    alt: 'Paper',
+    dark: true,
+    primary: true,
+    darkText: true
+};
+
+export const tourismObjThree = {
+    id : 'SectionThree',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '',
+    headline: "Tourism and the Saltillo",
+    description: "This photo shows tourists wearing Saltillo sarapes and sombreros while visiting Tijuana, Mexico. These items would have been purchased as souvenirs and gifts, increasing their recognition as symbols of Mexico.",
+    buttonLabel: "",
+    imgStart: false,
+    img: img1,
+    alt: 'Paper',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const tourismObjFour = {
+    id : 'SectionFour',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: '',
+    headline: "Tourism and the Saltillo",
+    description: "Oaxaca continues to be an important weaving center whose textiles are sold worldwide. Most Zapotec weavers now use synthetic dyes, rather than the traditional natural dyes, which cuts down on the work and makes production faster.",
+    buttonLabel: "",
+    imgStart: true,
+    img: img2,
+    alt: 'Paper',
+    dark: true,
+    primary: true,
+    darkText: true
+};
