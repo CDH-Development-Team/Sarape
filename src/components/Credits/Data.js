56b78a5826f8f8aabf98d8df554014983c86b618
eliacuna <elijahacuna@email.arizona.edu>
2021-11-03 06:46:36 -0700

    credits page created, needs more styling

---
+++ src/components/Credits/Data.js
@@ -0,0 +1,38 @@
+import img from "../../images/dyeing6.png";
+import img2 from "../../images/svg-2.svg";
+import img3 from "../../images/svg-3.svg";
+import img4 from "../../images/pintura.jpg";
+import img5 from "../../images/svg-5.svg";
+import img6 from "../../images/tourismInMexico.jpg";
+import img7 from "../../images/candids.jpg";
+import Icon1 from '../../images/svg-4.svg';
+import Icon2 from '../../images/svg-5.svg';
+import Icon3 from '../../images/svg-6.svg';
+import Icon4 from '../../textiles_png/3_textiles/AT-2020-16-2.png';
+import Icon5 from '../../textiles_png/3_textiles/AT-2020-16-3.png';
+import Icon6 from '../../textiles_png/3_textiles/AT-2020-16-4.png';
+import Icon7 from '../../textiles_png/3_textiles/AT-2020-16-5.png';
+import Icon8 from '../../textiles_png/3_textiles/AT-2020-16-6.png';
+import Icon9 from '../../textiles_png/3_textiles/AT-2020-16-7.png';
+
+export const homeObjOne = {
+    id : 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '',
+    headline: "",
+    description: '"A family of reds, from purples and violets to deep reds, pinks, corals, and oranges, is created with different amounts of cochineal, different mordants and modifiers, and various overdyes of cochineal and other natural dyestuffs.”',
+    description2: '',    
+    description3: '',
+    buttonLabel: "",
+    cap:"Keith Recker",
+    cap2:"True Colors: World Masters of Natural Dyles and Pigments,",
+    cap3:"Thrums Books 2019",
+    imgStart: false,
+    img: img,
+    alt: '',
+    dark: true,
+    primary: true,
+    darkText: false
+};
