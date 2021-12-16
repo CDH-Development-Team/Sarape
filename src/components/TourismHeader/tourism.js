7c7725f37043e9651babd804f8456182689507a9
eliacuna <elijahacuna@email.arizona.edu>
2021-12-16 01:11:47 -0700

    tourism restrucutred and alignment errors fixed

--- src/components/TourismHeader/tourism.js
+++ src/components/TourismHeader/tourism.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Button } from "../ButtonElement";
 
+import img1 from "../../images/tourismInMexico.jpg";
+import img2 from "../../images/candids.jpg";
+import img3 from "../../textiles_png/8_textiles/8425.jpg";
+import img4 from "../../textiles_png/8_textiles/educational_textile.jpg";
+import img5 from "../../images/8_ceramics.png";
+
 import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, ImgWrap2, Img, Img2, Img3, Artifact, Caption, TextHead } from './TourismElements';
 
 const TourismSection = ({lightBg, id, imgStart, topLine, lightText, headline, darkText,description,description2,description3, buttonLabel, img,alt, artifact, artifactAlt, artifactInfo, primary, dark, dark2}) => {
@@ -31,7 +37,73 @@ const TourismSection = ({lightBg, id, imgStart, topLine, lightText, headline, da
                                 <Caption darkText={darkText}>{description3}</Caption>
                             </ImgWrap2>
                         </Column2>
-                    </InfoRow>    
+                    </InfoRow>
+                    <InfoRow imgStart={true}>
+                        <Column1>
+                            <TextWrapper>
+                                <ImgWrap>
+                                    <Img src={img5} alt={artifactAlt}/>
+                                    <Caption darkText={darkText}>{"Ceramic Figurines, c. 1900-1920. ASM 156"}</Caption>
+                                </ImgWrap>
+                            </TextWrapper>
+                        </Column1>    
+                        <Column2>
+                            <Heading lightText={lightText}>{""}</Heading>
+                            <ImgWrap2>
+                                <TextHead>
+                                Mexican ceramic artists in the area of Guadalajara responded to the tourist market by creating miniature scenes of Mexican people from the 1800s. Often, the male figures who rode or worked with horses were depicted wearing Saltillo sarapes.
+                                <br/><br/>
+                                The attire of the two male figures wrapped in Saltillo sarapes indicates they are part of the upper class. The vaquero carries a sarape rolled up behind his saddle.
+                                </TextHead>
+                                {/* <Subtitle darkText={darkText}>{description}</Subtitle>
+                                <Subtitle darkText={darkText}>{description2}</Subtitle> */}
+                            </ImgWrap2>
+                        </Column2>
+                    </InfoRow>
+                    <InfoRow imgStart={imgStart}>
+                        <Column1>
+                            <TextWrapper>
+                                <ImgWrap>
+                                    <Img src={img3} style={{width:'50%', float: 'right'}} alt={'Tourist Photograph, May 5, 1920. Tijuana, Mexico. Loan from Tom Kieft'}/>
+                                    <Caption style={{float: 'right'}} darkText={darkText}>{'Rainbow-striped Saltillo Sarape c. 1900, Wool, Cotton, and Synthetic Dyes, ASM Purchase from Nelle A. Dermont, 1919, 8425'}</Caption>
+                                </ImgWrap>
+                            </TextWrapper>
+                        </Column1>    
+                        <Column2>
+                            <Heading lightText={lightText}>{""}</Heading>
+                            <ImgWrap2>
+                                <TextHead>
+                                This photo shows tourists wearing Saltillo sarapes and sombreros while visiting Tijuana, Mexico. These items would have been purchased as souvenirs and gifts, increasing their recognition as symbols of Mexico.
+                                </TextHead>
+                                {/* <Subtitle darkText={darkText}>{description}</Subtitle>
+                                <Subtitle darkText={darkText}>{description2}</Subtitle> */}
+                                <Img2 src={img1} alt={alt}/>
+                                <Caption darkText={darkText}>{'Tourist Photograph, May 5, 1920. Tijuana, Mexico. Loan from Tom Kieft'}</Caption>
+                            </ImgWrap2>
+                        </Column2>
+                    </InfoRow>
+                    <InfoRow imgStart={true}>
+                        <Column1>
+                            <TextWrapper>
+                                <ImgWrap>
+                                    <Img src={img4} style={{width:'50%', float: 'right'}} alt={'Tourist Photograph, May 5, 1920. Tijuana, Mexico. Loan from Tom Kieft'}/>
+                                    <Caption style={{float: 'right'}} darkText={darkText}>{'Morning Stars. Zapotec Textile. Wool, Cotton, and Synthetic Dyes. ASM Purchase For Education Collection. 2020'}</Caption>
+                                </ImgWrap>
+                            </TextWrapper>
+                        </Column1>    
+                        <Column2>
+                            <Heading lightText={lightText}>{""}</Heading>
+                            <ImgWrap2>
+                                <TextHead>
+                                Oaxaca continues to be an important weaving center whose textiles are sold worldwide. Most Zapotec weavers now use synthetic dyes, rather than the traditional natural dyes, which cuts down on the work and makes production faster.
+                                </TextHead>
+                                {/* <Subtitle darkText={darkText}>{description}</Subtitle>
+                                <Subtitle darkText={darkText}>{description2}</Subtitle> */}
+                                <Img2 src={img2} style={{width:'85%'}} alt={alt}/>
+                                <Caption darkText={darkText}>{"Zapotec Textiles on Sale at the Arizona State Museum's Southwest Indian Art Fair. Alanah K. Tupponce, Photographer, 2011."}</Caption>
+                            </ImgWrap2>
+                        </Column2>
+                    </InfoRow>
                 </InfoWrapper>   
             </InfoContainer> 
         </>
